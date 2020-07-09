053b313afedb7dc57ccd44686039ba8a6b06e6e9
didierdelhaisse <did35@yahoo.com>
2020-07-09 09:47:49 +0200

    Section 5/ Alert added in MenuDetailView

--- Pizza/MenuView/MenuDetailView.swift
+++ Pizza/MenuView/MenuDetailView.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct MenuDetailView: View {
     @EnvironmentObject var settings: UserPreferences
     @ObservedObject var orderModel: OrderModel
+    @State private var didOrder = false
     
     var menuItem: MenuItem
     var formattedPrice: String {
@@ -18,6 +19,7 @@ struct MenuDetailView: View {
     }
     func addItem(){
         orderModel.add(menuID: menuItem.id)
+        didOrder = true
     }
     
 
@@ -66,6 +68,9 @@ struct MenuDetailView: View {
                     .background(Color("G4"))
                         .foregroundColor(Color("IP"))
                         .cornerRadius(5)
+                }
+                .alert(isPresented: $didOrder) {
+                    Alert(title: Text("Pizza Ordered"), message: Text("You ordered a " + self.menuItem.name), dismissButton: nil)
                 }
                 Spacer()
             }
